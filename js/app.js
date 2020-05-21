8471e38143c487b4745680b225e09a1f449d73b0
AhmadKheder <ahmadkhederabdulatef@gmail.com>
2020-05-22 00:13:39 +0300

    msg

---
+++ js/app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+function checkboxManipulate(form) {
+    var chBX;
+    var slectParagraph 
+    var cb = document.getElementById('formy').getElementsByTagName('input');
+    // console.log(cb);
+    for (var i = 0; i < cb.length; i++) {
+        if (cb[i].checked == true) {
+             chBX = cb[i].id;
+             slectParagraph = document.getElementsByName(chBX);
+
+            // var del = document.createElement('del');
+            console.log(slectParagraph);
+            // slectParagraph[i].innerHTML = `<del> ${slectParagraph[i].textContent}<del>`
+            slectParagraph[i].innerHTML = `<del> ${slectParagraph[0].textContent}</del>`
+            // del.textContent = slectParagraph[i].innerHTML;
+            // slectParagraph[i].textContent ='';
+            // slectParagraph[i].appendChild(del);
+        }
+    }
+}
+
