748052cff6b2414d5d98545d780c7c1a157f3e40
AhmadKheder <54719438+AhmadKheder@users.noreply.github.com>
2020-05-22 01:04:28 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,19 +4,13 @@ function checkboxManipulate(form) {
     var chBX;
     var slectParagraph 
     var cb = document.getElementById('formy').getElementsByTagName('input');
-    // console.log(cb);
     for (var i = 0; i < cb.length; i++) {
         if (cb[i].checked == true) {
              chBX = cb[i].id;
              slectParagraph = document.getElementsByName(chBX);
 
-            // var del = document.createElement('del');
-
-            // slectParagraph[i].innerHTML = `<del> ${slectParagraph[i].textContent}<del>`
             slectParagraph[i].innerHTML = `<del> ${slectParagraph[i].textContent}</del>`
-            // del.textContent = slectParagraph[i].innerHTML;
-            // slectParagraph[i].textContent ='';
-            // slectParagraph[i].appendChild(del);
+           
         }
     }
 }
